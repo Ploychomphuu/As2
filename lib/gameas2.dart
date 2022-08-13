fa920ba6a1cdbc53db3c0a196946f729ca76d8f6
Ploychomphu Khrutcha <may.khrutcha@gmail.com>
2022-08-13 23:49:42 +0700

    Initial commit

---
+++ lib/gameas2.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+
+class Game {
+  int ans = 0;
+  var List = <int>[];
+
+  Game([int maxRandom = 100]) {
+   Random r = Random();
+   ans = r.nextInt(maxRandom)+1;
+
+  }
+  int count=1;
+  int doguess(int num) {
+    if (num > ans) {
+      print("➜ $num is TOO HIGH!!");
+      print("──────────────────────────────────────────────");
+      count++;
+      List.add(num);
+      return 0;
+    } else if (num < ans) {
+      print("➜ $num is TOO LOW!!");
+      print("──────────────────────────────────────────────");
+      count++;
+      List.add(num);
+      return 0;
+    } else {
+      print("➜ $num is CORRECT ❤, total guesses: $count");
+      count = 1;
+      List.add(num);
+      return 1;
+    }
+  }
+  void length(){
+    print("${List.length} in a row");
+  }
+}
+
+
+
+
+
+
+
+
---
+++ lib/newas2.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+import 'gameas2.dart';
+
+
+void main() {
+  var game = Game();
+  int result = 0;
+  var max;
+
+  print("++++++++++++++++++++++++++++++++++++++++++++++");
+  print("                PLAY THE GAME                 ");
+  print("++++++++++++++++++++++++++++++++++++++++++++++");
+
+  while(true){
+    print("Please input maxrandom: ");
+    var input = stdin.readLineSync();
+    var count;
+
+    if(input == "") {
+      max = Game();
+    }else{
+      var guess = int.tryParse(input!);
+      count = guess;
+      max = Game(guess!);
+    }
+
+  do {
+    stdout.write("Please guess the number between 1 and $count: ");
+    var input = stdin.readLineSync();
+    var guess = int.tryParse(input!);
+
+    if (guess == null) {
+      continue;
+    }
+    result = max.doguess(guess);
+  } while (result != 1);
+  max.length();
+    print("You want to play game again?");
+    stdout.write("Please enter Y or N: ");
+    var play = stdin.readLineSync();
+
+      if (input == "N") {
+      print("++++++++++++++++++++++++++++++++++++++++++++++");
+      print("                  END GAME                    ");
+      print("++++++++++++++++++++++++++++++++++++++++++++++");
+      break;
+
+    } else {
+        print("Please enter Y or N: ");
+        continue;
+      }
+  }
+}
